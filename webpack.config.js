fef6eb67632ead121496c2e7cecd2059bd2c458a
Yoav Vainrich <yoavain@gmail.com>
2019-09-22 00:20:40 +0300

    Try to use webpack instead of committed node_modules

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	entry: './compiled/main.js',
+	target: 'node',
+	node: {
+		__filename: true,
+		__dirname: true
+	},
+	output: {
+		path: path.join(__dirname, 'lib'),
+		filename: 'main.js'
+	},
+	plugins: [
+		new webpack.IgnorePlugin(/\.(css|less)$/)
+	],
+	devtool: 'sourcemap'
+};
